1b2b73861d2eb83ab852bf7e35d460c8ae8c7638
Tim Culverhouse <tim@timculverhouse.com>
2025-12-12 02:20:02 -0600

    release: v0.3.1

--- build.zig
+++ build.zig
@@ -283,7 +283,7 @@ pub fn build(b: *std.Build) void {
     enable_service_step.dependOn(b.getInstallStep());
 }
 
-const version_base = "0.1.0";
+const version_base = "0.3.1";
 
 fn getVersion(b: *std.Build) []const u8 {
     var code: u8 = undefined;
